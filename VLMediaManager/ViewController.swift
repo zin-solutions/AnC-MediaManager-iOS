63233547388aa6552945e7203f7569f348fa53c9
Hussein Al Mawla <info@hmawla.com>
2020-07-09 20:18:45 +0300

    Initial Commit

---
+++ VLMediaManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VLMediaManager
+//
+//  Created by Hussein AlMawla on 7/9/20.
+//  Copyright © 2020 Hussein AlMawla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
